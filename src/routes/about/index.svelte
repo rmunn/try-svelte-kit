5bfc62099474beb911d1436c4b5beac12c94eedd
Robin Munn <rmunn@pobox.com>
2021-01-06 09:08:17 +0700

    Show off slots and params in Markdown
    Because the Markdown is pre-processed just like any other Svelte
    component, we can use Svelte features like slots and params inside the
    Markdown file. Easy-peasy, lemon squeezy!

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,9 +1,13 @@
 <script>
     import Nav from '$components/Nav.svelte';
     import About from '$pages/about.md';
+
+    let number = 42;
 </script>
 
 <main>
     <Nav/>
-    <About/>
+    <About param={36+2}>
+        <p>Inserted {number} into the about page.</p>
+    </About>
 </main>
