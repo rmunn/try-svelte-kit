f9935142d19055710480691c0454ca7455cdb4a7
Robin Munn <rmunn@pobox.com>
2021-01-08 11:19:41 +0700

    Add comment in snowpack config re manifest issue

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,6 +2,8 @@
 module.exports = {
 	extends: '@sveltejs/snowpack-config',
 	plugins: ['@snowpack/plugin-typescript'],
+	// NOTE: Commenting out the above line fixed the manifest issue in Snowpack 2.18.0 - 2.18.4
+	// Snowpack 2.18.5 is first version to include https://github.com/snowpackjs/snowpack/pull/2006 which fixes the issue
 	mount: {
 		'src/pages': '/_pages',
 		'src/components': '/_components'
