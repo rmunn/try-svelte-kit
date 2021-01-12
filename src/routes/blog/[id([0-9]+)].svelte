0455169a4f682ee3e5fb51352c6e67f0aa9d5e88
Robin Munn <rmunn@pobox.com>
2021-01-12 12:56:31 +0700

    Sample of fetching blog contents in load()
    This shows an example of how this would play out in a real app.

--- src/routes/blog/[id([0-9]+)].svelte
+++ src/routes/blog/[id([0-9]+)].svelte
@@ -1,11 +1,20 @@
 <script context="module">
-    export async function load({page: {params: { id }}}) {
-        return { props: { id } };
+    export async function load({page, fetch}) {
+        const { id } = page.params;
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        const json = await response.json();
+        console.log('Response', json);
+        const { title, body } = json;
+        return { props: { id, title, body } };
     }
 </script>
 
 <script>
     export let id;
+    export let title;
+    export let body;
 </script>
 
 <p>Blog entry {id} can go here</p>
+<p>Title: {title}</p>
+<p>Body: {body}</p>
