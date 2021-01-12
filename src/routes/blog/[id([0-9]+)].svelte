fcaaa0505739ec48c4222dba25704236a3f17d0e
Robin Munn <rmunn@pobox.com>
2021-01-12 12:37:01 +0700

    Better blog page sample
    The load() function doesn't need all those console.log calls now that
    we're handling its return value correctly.

--- src/routes/blog/[id([0-9]+)].svelte
+++ src/routes/blog/[id([0-9]+)].svelte
@@ -1,22 +1,13 @@
 <script context="module">
-    export async function load({page}) {
-        console.log(`Would load blog ID ${page}`, page, page.params);
-        const { id } = page.params;
-        // const x = {id: page.params.id};
-        console.log(id)
-        const x = { id };
-        console.log(x);
-        const p = new Promise(resolve => resolve(x));
-        console.log(p);
-        return { props: { id: 5, foo: "bar" } };
+    export async function load(context) {
+        console.log(`Page context is`, context);
+        const { id } = context.page.params;
+        return { props: { id: id } };
     }
 </script>
 
 <script>
     export let id;
-    export let foo;
-
-    console.log("Props:", $$props);
 </script>
 
-<p>Blog {id} and {foo}</p>
+<p>Blog entry {id} can go here</p>
