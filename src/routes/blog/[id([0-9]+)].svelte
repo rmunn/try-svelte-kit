64aa44c7745ba0a4edf7c8c71d1a0301fa0b500e
Robin Munn <rmunn@pobox.com>
2021-01-12 12:24:08 +0700

    First attempt at using route parameters
    This is how it would have been done in Sapper (though the function name
    was preload() in Sapper and is load() in Svelte Kit). This doesn't
    appear to work, but stay tuned.

---
+++ src/routes/blog/[id([0-9]+)].svelte
@@ -0,0 +1,22 @@
+<script context="module">
+    export async function load({page}) {
+        console.log(`Would load blog ID ${page}`, page, page.params);
+        const { id } = page.params;
+        // const x = {id: page.params.id};
+        console.log(id)
+        const x = { id };
+        console.log(x);
+        const p = new Promise(resolve => resolve(x));
+        console.log(p);
+        return { id: 5, foo: "bar" };
+    }
+</script>
+
+<script>
+    export let id;
+    export let foo;
+
+    console.log("Props:", $$props);
+</script>
+
+<p>Blog {id} and {foo}</p>
